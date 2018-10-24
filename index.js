4d7b1814e7fd6d5aa3a56461ddd1bcef3319306e
unknown <ruiz.james12@yahoo.com>
2018-10-24 09:17:44 -0700

    "Created new DB, solved bug, implemented POST"

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
 
 const server = express()
 
@@ -8,6 +13,29 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('Homepage')
+})
+
+server.get('/api/zoos', (req, res) => {
+    db('zoos')
+    .then(zoos => {
+        res.status(200).json(zoos)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+server.post('/api/zoos', (req, res) => {
+    const zoo = req.body;
+
+    db.insert(zoo)
+    .into('zoos')
+    .then(ids => {
+        res.status(201).json(ids)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
 })
 
 server.listen(9000, () => console.log('\nAPI is running on 9000\n'))
--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,10 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './Zoos.db'
+      filename: './db.db3'
     },
-    useNullAsDefault: true,
   },
 };
